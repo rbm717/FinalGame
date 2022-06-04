41e8f2dcc3bd759a9a134bafebf37f5dc7877321
thebeardlessdwarf <vinivss@hotmail.com>
2022-06-04 13:23:28 -0700

    Update Menu.js

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -9,6 +9,8 @@ class Menu extends Phaser.Scene{
 
     create(){
         keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        this.add.text(game.config.width/2, game.config.height/2, 'W A S D to move', {fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif'}).setOrigin(0, 0);
+        this.add.text(game.config.width/2, game.config.height/2, 'F to fire   X to drop weapon', {fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif'}).setOrigin(0.25, 0);
         this.add.text(game.config.width/2, game.config.height/2, 'Space to start', {fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif'}).setOrigin(0.5, 0);
     }
 
