92008089797d2c9475e966c436131f71bc0d3b60
rbm717 <47369758+rbm717@users.noreply.github.com>
2022-06-03 18:47:21 -0700

    Floortile Tilemaps

---
+++ assets/floortile_converted.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="floortile_converted" tilewidth="16" tileheight="16" tilecount="72" columns="12">
+ <image source="../../../../../../Downloads/floortile_converted.png" width="192" height="96"/>
+</tileset>
