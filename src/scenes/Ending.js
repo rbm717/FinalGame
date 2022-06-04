a471eaa0c7d82e1037d06e3d50a7b9d80fafec44
thebeardlessdwarf <vinivss@hotmail.com>
2022-06-04 13:39:26 -0700

    basic ending scene added

---
+++ src/scenes/Ending.js
@@ -0,0 +1,22 @@
+class Ending extends Phaser.Scene{
+    constructor() {
+        super("EndingScene");
+    }
+
+    preload(){
+        // Put Music and art here
+    }
+
+    create(){
+        keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        this.add.text(game.config.width/2, game.config.height/4, 'YOU ARE DEAD', {fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif'}).setOrigin(0.5, 0);
+        
+        this.add.text(game.config.width/2, game.config.height/2, 'Press Space to try again', {fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif'}).setOrigin(0.5, 0);
+    }
+
+    update(){
+        if(Phaser.Input.Keyboard.JustDown(keySpace) ){
+            this.scene.start('level1');
+        }
+    }
+}
