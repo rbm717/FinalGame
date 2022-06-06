21b4e140b2e1ee1245bed03a06c9a8ffb4b4f605
thebeardlessdwarf <vinivss@hotmail.com>
2022-06-05 17:12:12 -0700

    Level 2 Tilesheet Made

---
+++ assets/BGvamp.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="BGvamp" tilewidth="32" tileheight="32" tilecount="126" columns="14">
+ <image source="backgroundvamp.png" width="448" height="288"/>
+</tileset>
---
+++ assets/BasicStuff.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="BasicStuff" tilewidth="32" tileheight="32" tilecount="12" columns="3">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <image source="spritesheet.png" width="96" height="144"/>
+ <tile id="6">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
